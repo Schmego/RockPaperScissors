d6caca70a84c2e83beb289ab88b12fac82f0d82e
Rego <0240179@students.dadeschools.net>
2018-03-12 12:01:56 -0400

    Starting project

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 0240179
+ * Date: 3/12/2018
+ * Time: 11:53 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace RockPaperScissors
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
